f05f6d1aa2428292a8feafbdf3fffafca9e1d0a6
Nic <nic.ehmayer92@gmail.com>
2018-01-23 02:16:15 +0000

    Added Step 4: Printing with callbacks.

---
+++ callbacks.js
@@ -0,0 +1,29 @@
+function getHTML (options, callback) {
+  var https = require('https');
+
+  https.get(options, function(response) {
+    var bufferedData = '';
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      bufferedData += '+--------------------+' + data + '+--------------------+';
+    });
+
+    response.on('end', function() {
+      callback(bufferedData);
+      console.log('End of data stream...');
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
